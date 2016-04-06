032b76787f5e47b5a9df65a3d56ad80b04a6bf37
Johnny Luangphasy <johnny@jluangphasy.com>
2016-04-06 15:28:03 -0700

    Added body class for each page

--- javascripts/yvonne.js
+++ javascripts/yvonne.js
@@ -1,24 +1,3 @@
-//CHART ON CHRISTMAS PAGE
-
-
-    var toyData = {
-    	labels : ["2010","2011","2012","2013","2014","2015"],
-    	datasets : [
-    		{
-    			fillColor : "rgba(97,182,255,.4)",
-    			strokeColor : "#337ab7",
-    			pointColor : "#fff",
-    			pointStrokeColor : "#9DB86D",
-    			data : [160,180,250,290,340,380]
-    		}
-    	]
-    }
-
-    var toys = document.getElementById('toyChart').getContext('2d');
-        new Chart(toys).Line(toyData);
-
-
-
 var userInput = {};
 
 
