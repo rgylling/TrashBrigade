5707b6d27b05dc6427ae8a5207a81effc3dbe5e8
Randy Gylling <MikeGylling@yahoo.com>
2016-04-07 15:30:34 -0700

    refactored some code and changed some files

--- js/randy.js
+++ js/randy.js
@@ -1,25 +1,3 @@
-//CHART ON CHRISTMAS PAGE
-var toyData = {
-  labels: ['2010', '2011', '2012', '2013', '2014', '2015'],
-	datasets: [
-  	{
-  		fillColor: 'rgba(160, 193, 222, .4)',
-  		strokeColor: '#337ab7',
-  		pointColor: '#fff',
-  		pointStrokeColor: '#9db86d',
-  		data: [160, 180, 250, 290, 340, 380]
-  	}
-	]
-};
-
-var toyOptions = {
-  responsive: true
-};
-
-var toys = document.getElementById('toyChart').getContext('2d');
-new Chart(toys).Line(toyData, toyOptions);
-// END CHART
-
 
 var dd = 3;
 var cnt = -1;
