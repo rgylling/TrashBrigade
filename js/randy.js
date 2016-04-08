0eecf6e1d8eedf7b953ec23440d81b5f729103c3
Randy Gylling <MikeGylling@yahoo.com>
2016-04-08 11:41:37 -0700

    I refactored my code to include objects

--- js/randy.js
+++ js/randy.js
@@ -1,6 +1,46 @@
 
 var dd = 3;           // variable based off of how many images
 var cnt = -1;         // variable counting through the index
+
+
+       /*--------------------------------------------
+             My object holding my image paths
+        -------------------------------------------- */
+
+var myObj = {
+imageDir: 'img/kids/',
+imageDir2: 'img/xmas/',
+imgId1: document.getElementById('imgone'),
+imgId2: document.getElementById('imgtwo'),
+imgId3: document.getElementById('imgthree'),
+imgId4: document.getElementById('imgfour'),
+imgId5: document.getElementById('imgfive'),     //About img Ids
+imgId6: document.getElementById('imgsix'),
+imgId7: document.getElementById('imgseven'),
+xmasId1: document.getElementById('xmasone'),
+xmasId2: document.getElementById('xmastwo'),
+xmasId3: document.getElementById('xmasthree'),    //Xmas img Ids
+xmasId4: document.getElementById('xmasfour'),
+xmasId5: document.getElementById('xmasfive'),
+xmasId6: document.getElementById('xmassix'),
+xmasId7: document.getElementById('xmasseven'),
+images1: ['Kids11.jpg','Kids12.jpg','Kids01.jpg'],
+images2: ['Kids13.jpg','Kids16.jpg','Kids04.jpg'],
+images3: ['Kids07.jpg','Kids09.jpg','Kids02.jpg'],    //Aboutme Img Paths
+images4: ['Kids10.jpg','Kids14.jpg','Kids03.jpg'],
+images5: ['Kids15.jpg','Kids18.jpg','Kids05.jpg'],
+images6: ['Kids17.jpg','Kids19.jpg','Kids06.jpg'],
+images7: ['Kids20.jpg','Kids21.jpg','Kids08.jpg'],
+xmas1: ['XMas10.jpg','XMas12.jpg','XMas02.jpg'],
+xmas2: ['XMas13.jpg','XMas14.jpg','XMas03.jpg'],
+xmas3: ['XMas07.jpg','XMas08.jpg','XMas01.jpg'],     //Xmas Img Paths
+xmas4: ['XMas11.jpg','XMas16.jpg','XMas04.jpg'],
+xmas5: ['XMas18.jpg','XMas21.jpg','XMas05.jpg'],
+xmas6: ['XMas15.jpg','XMas17.jpg','XMas06.jpg'],
+xmas7: ['XMas19.jpg','XMas20.jpg','XMas09.jpg']
+}
+
+
 function chng() {
 
            /*--------------------------------
@@ -15,59 +55,6 @@ function chng() {
     $(".fadeThis").fadeTo(2000,1);
 
 
-
-             /*---------------------------
-                       Image Id's
-              -------------------------- */
-
-
-    var img1 = document.getElementById('imgone');    // match image ID
-    var img2 = document.getElementById('imgtwo');
-    var img3 = document.getElementById('imgthree');
-    var img4 = document.getElementById('imgfour');
-    var img5 = document.getElementById('imgfive');
-    var img6 = document.getElementById('imgsix');
-    var img7 = document.getElementById('imgseven');
-    var xmas1 = document.getElementById('xmasone');   // match image ID
-    var xmas2 = document.getElementById('xmastwo');
-    var xmas3 = document.getElementById('xmasthree');
-    var xmas4 = document.getElementById('xmasfour');
-    var xmas5 = document.getElementById('xmasfive');
-    var xmas6 = document.getElementById('xmassix');
-    var xmas7 = document.getElementById('xmasseven');
-
-    var imageDir = 'img/kids/';       // change to match images folder
-    var imageDir2 = 'img/xmas/';
-
-
-              /*----------------------------
-                 About Page Image Directory
-              ---------------------------- */
-
-
-    var images1 = ['Kids11.jpg','Kids12.jpg','Kids01.jpg'];
-    var images2 = ['Kids13.jpg','Kids16.jpg','Kids04.jpg']
-    var images3 = ['Kids07.jpg','Kids09.jpg','Kids02.jpg']
-    var images4 = ['Kids10.jpg','Kids14.jpg','Kids03.jpg']
-    var images5 = ['Kids15.jpg','Kids18.jpg','Kids05.jpg']
-    var images6 = ['Kids17.jpg','Kids19.jpg','Kids06.jpg']
-    var images7 = ['Kids20.jpg','Kids21.jpg','Kids08.jpg']
-
-
-             /*---------------------------
-               Xmas Page Image Directory
-            ---------------------------- */
-
-
-    var xmasOne = ['XMas10.jpg','XMas12.jpg','XMas02.jpg'];
-    var xmasTwo = ['XMas13.jpg','XMas14.jpg','XMas03.jpg']
-    var xmasThree = ['XMas07.jpg','XMas08.jpg','XMas01.jpg']
-    var xmasFour = ['XMas11.jpg','XMas16.jpg','XMas04.jpg']
-    var xmasFive = ['XMas18.jpg','XMas21.jpg','XMas05.jpg']
-    var xmasSix = ['XMas15.jpg','XMas17.jpg','XMas06.jpg']
-    var xmasSeven = ['XMas19.jpg','XMas20.jpg','XMas09.jpg']
-
-
     cnt++;     //adding one to the count to go through the array index
 
 
@@ -77,23 +64,23 @@ function chng() {
 
 
     if (cnt < dd) {
-      if (img1){
-        img1.src = imageDir + images1[cnt];
-        img2.src = imageDir + images2[cnt];
-        img3.src = imageDir + images3[cnt];
-        img4.src = imageDir + images4[cnt];
-        img5.src = imageDir + images5[cnt];
-        img6.src = imageDir + images6[cnt];
-        img7.src = imageDir + images7[cnt];
+      if (myObj.imgId1){
+      myObj.imgId1.src = myObj.imageDir + myObj.images1[cnt];
+      myObj.imgId2.src = myObj.imageDir + myObj.images2[cnt];
+      myObj.imgId3.src = myObj.imageDir + myObj.images3[cnt];
+      myObj.imgId4.src = myObj.imageDir + myObj.images4[cnt];
+      myObj.imgId5.src = myObj.imageDir + myObj.images5[cnt];
+      myObj.imgId6.src = myObj.imageDir + myObj.images6[cnt];
+      myObj.imgId7.src = myObj.imageDir + myObj.images7[cnt];
       };
-      if (xmas1){
-        xmas1.src = imageDir2 + xmasOne[cnt];
-        xmas2.src = imageDir2 + xmasTwo[cnt];
-        xmas3.src = imageDir2 + xmasThree[cnt];
-        xmas4.src = imageDir2 + xmasFour[cnt];
-        xmas5.src = imageDir2 + xmasFive[cnt];
-        xmas6.src = imageDir2 + xmasSix[cnt];
-        xmas7.src = imageDir2 + xmasSeven[cnt];
+      if (myObj.xmasId1){
+      myObj.xmasId1.src = myObj.imageDir2 + myObj.xmas1[cnt];
+      myObj.xmasId2.src = myObj.imageDir2 + myObj.xmas2[cnt];
+      myObj.xmasId3.src = myObj.imageDir2 + myObj.xmas3[cnt];
+      myObj.xmasId4.src = myObj.imageDir2 + myObj.xmas4[cnt];
+      myObj.xmasId5.src = myObj.imageDir2 + myObj.xmas5[cnt];
+      myObj.xmasId6.src = myObj.imageDir2 + myObj.xmas6[cnt];
+      myObj.xmasId7.src = myObj.imageDir2 + myObj.xmas7[cnt];
       };
     }
 
@@ -104,23 +91,23 @@ function chng() {
 
 
     else if (cnt == dd) {
-      if (img1){
-        img1.src = imageDir + images1[0];
-        img2.src = imageDir + images2[0];
-        img3.src = imageDir + images3[0];
-        img4.src = imageDir + images4[0];
-        img5.src = imageDir + images5[0];
-        img6.src = imageDir + images6[0];
-        img7.src = imageDir + images7[0];
+      if (myObj.imgId1){
+      myObj.imgId1.src = myObj.imageDir + myObj.images1[0];
+      myObj.imgId2.src = myObj.imageDir + myObj.images2[0];
+      myObj.imgId3.src = myObj.imageDir + myObj.images3[0];
+      myObj.imgId4.src = myObj.imageDir + myObj.images4[0];
+      myObj.imgId5.src = myObj.imageDir + myObj.images5[0];
+      myObj.imgId6.src = myObj.imageDir + myObj.images6[0];
+      myObj.imgId7.src = myObj.imageDir + myObj.images7[0];
       };
-      if (xmas1){
-        xmas1.src = imageDir2 + xmasOne[0];
-        xmas2.src = imageDir2 + xmasTwo[0];
-        xmas3.src = imageDir2 + xmasThree[0];
-        xmas4.src = imageDir2 + xmasFour[0];
-        xmas5.src = imageDir2 + xmasFive[0];
-        xmas6.src = imageDir2 + xmasSix[0];
-        xmas7.src = imageDir2 + xmasSeven[0];
+      if (myObj.xmasId1){
+      myObj.xmasId1.src = myObj.imageDir2 + myObj.xmas1[0];
+      myObj.xmasId2.src = myObj.imageDir2 + myObj.xmas2[0];
+      myObj.xmasId3.src = myObj.imageDir2 + myObj.xmas3[0];
+      myObj.xmasId4.src = myObj.imageDir2 + myObj.xmas4[0];
+      myObj.xmasId5.src = myObj.imageDir2 + myObj.xmas5[0];
+      myObj.xmasId6.src = myObj.imageDir2 + myObj.xmas6[0];
+      myObj.xmasId7.src = myObj.imageDir2 + myObj.xmas7[0];
       };
         cnt = 0;
     }
